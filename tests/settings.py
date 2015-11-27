b6544ac4b3ded8991053251de21951de82c117e7
Adam Chainz <adam@adamj.eu>
2015-11-27 13:39:48 +0000

    Add Django template_tests

--- tests/settings.py
+++ tests/settings.py
@@ -17,12 +17,14 @@ DATABASES = {
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_speedboost',
+    'template_tests',
     'testapp'
 )
 
